7949560d5c53497d64195b96dadaa54e71f5c932
unknown <podinarares@gmail.com>
2021-01-21 15:11:43 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebae: {
+    apiKey: "AIzaSyC5EH3v97hCIshmzm3_uC_-SlHceWcQtwk",
+    authDomain: "bt-challenge.firebaseapp.com",
+    projectId: "bt-challenge",
+    storageBucket: "bt-challenge.appspot.com",
+    messagingSenderId: "385617790371",
+    appId: "1:385617790371:web:e683b68e582a22bf66c616"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
